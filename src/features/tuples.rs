9c85f064fb874c9af742a13531a74ab3252dc771
Onaconnapuna <cjgermain12@gmail.com>
2024-05-31 22:02:56 -0400

    tuple struct

--- src/features/tuples.rs
+++ src/features/tuples.rs
@@ -1,29 +1,32 @@
-struct Tuple(f64, f64, f64, f64);
+pub mod tuples {
 
-trait Point {
-    fn is_point(&self) -> bool;
-}
+pub struct Tuple(pub f64, pub f64, pub f64, pub f64);
 
-impl Point for Tuple { 
-    fn is_point(&self) -> bool {
-        if self.3 == 1.0 { return true } else { return false };
+    trait Point {
+        fn is_point(&self) -> bool;
+    }
+
+    impl Point for Tuple { 
+        fn is_point(&self) -> bool {
+            if self.3 == 1.0 { return true } else { return false };
+        }
+    }
+
+    trait Vector {
+        fn is_vector(&self) -> bool;
+    }
+
+    impl Vector for Tuple { 
+        fn is_vector(&self) -> bool {
+            if self.3 == 0.0 { return true } else { return false };
+        }
     }
 }
-
-trait Vector {
-    fn is_vector(&self) -> bool;
-}
-
-impl Vector for Tuple { 
-    fn is_vector(&self) -> bool {
-        if self.3 == 0.0 { return true } else { return false };
-    }
-}
-fn main() {
+/*fn main() {
     let a_point = Tuple(4.3, -4.2, 3.1, 1.0);
     let a_vector = Tuple(4.3, -4.2, 3.1, 0.0);
     println!("a point is a Point: {}", a_point.is_point());
     println!("a point is a Vector: {}", a_point.is_vector());
     println!("a vector is a Point: {}", a_vector.is_point());
     println!("a vector is a Point: {}", a_vector.is_vector());
-}
+}*/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main {
+use crate::features::tuples::tuples::Tuple;
+
+pub mod features;
+
+fn main() {
     println!("Hello World");
+    let a_point = Tuple(4.2, -4.0, -4.3, 1.0);
 }
